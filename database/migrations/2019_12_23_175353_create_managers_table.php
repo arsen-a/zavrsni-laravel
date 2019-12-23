50c9ebf5c4450b0750faa5d54d4cc58f40c1a621
Arsen Sekularac <arsen.s@live.com>
2019-12-23 20:43:39 +0100

    modeli, kontroleri, migracije, shops ruta kreirana

---
+++ database/migrations/2019_12_23_175353_create_managers_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateManagersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('managers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('image');
+            $table->string('email');
+            $table->unsignedBigInteger('shop_id')->default(null);
+            $table->timestamps();
+        });
+
+        Schema::table('shops', function (Blueprint $table) {
+            $table->foreign('manager_id')->references('id')->on('managers');
+        });
+
+        Schema::table('managers', function (Blueprint $table) {
+            $table->foreign('shop_id')->references('id')->on('shops');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('managers');
+    }
+}
