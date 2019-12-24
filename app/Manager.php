b26302e0204c529a106366bd0bf33f4685155e9a
Arsen Sekularac <arsen.s@live.com>
2019-12-24 13:57:36 +0100

    shop-manager relacija

--- app/Manager.php
+++ app/Manager.php
@@ -2,9 +2,15 @@
 
 namespace App;
 
+use App\Shop;
 use Illuminate\Database\Eloquent\Model;
 
 class Manager extends Model
 {
     protected $guarded = ['id'];
+
+    public function store() 
+    {
+        return $this->hasOne(Shop::class);
+    }
 }
--- app/Shop.php
+++ app/Shop.php
@@ -2,9 +2,15 @@
 
 namespace App;
 
+use App\Manager;
 use Illuminate\Database\Eloquent\Model;
 
 class Shop extends Model
 {
     protected $guarded = ['id'];
+
+    public function manager()
+    {
+        return $this->belongsTo(Manager::class);
+    }
 }
